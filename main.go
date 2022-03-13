13f9d31ed1155523bf6e6e076dd9e0d8e3aad27c
Phan Trong <trongpq@runsystem.net>
2022-03-13 21:37:02 +0700

    add make file

--- main.go
+++ main.go
@@ -3,11 +3,18 @@ package main
 import (
 	"gin-mongo-api/configs"
 	"gin-mongo-api/routes"
+	"log"
+	"os"
 
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
+	port := os.Getenv("PORT")
+
+	if port == "" {
+		log.Fatal("$PORT must be set")
+	}
 	router := gin.Default()
 
 	//run database
@@ -16,5 +23,5 @@ func main() {
 	//routes
 	routes.UserRoute(router) //add this
 
-	router.Run(":80")
+	router.Run(":" + port)
 }
